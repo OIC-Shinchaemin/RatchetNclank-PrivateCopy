2537f7e0a53bcec5585714fbfc778ced4af3b071
OiC-b8073-Ochi <b8073@oic.jp>
2021-04-12 07:53:20 +0900

    ボスの攻撃を作成、フラグ用のコライダーが反応しない

---
+++ ClientProject/Unity/Assets/Scripts/BossEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEvent : MonoBehaviour
+{
+    public GameObject player;
+    public bool bBoss = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            bBoss = true;
+        }
+    }
+}
---
+++ ClientProject/Unity/Assets/Scripts/GhetsisChild.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhetsisChild : MonoBehaviour
+{
+    //public Vector2 speed = new Vector2(0.05f, 0.05f);
+    float speed = 5f;
+    private Vector2 Position;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        //gameObject.transform.position = transform.position;
+        //transform.Translate(speed, speed, 0);
+        float angleDir = transform.eulerAngles.z * (Mathf.PI / 180.0f);
+        Vector3 dir = new Vector3(Mathf.Cos(angleDir), Mathf.Sin(angleDir), 0.0f);
+
+        transform.position += dir * speed * Time.deltaTime *-1;
+    }
+
+}
