c97bbf8966776cbe39891bdd7962998bfe4c80ce
DESKTOP-1T5ODT0\USER <jacktoaburaage@gmail.com>
2021-04-21 11:51:46 +0900

    学校で作業するためコミット

--- ClientProject/Unity/Assets/Scripts/Eagle.cs
+++ ClientProject/Unity/Assets/Scripts/Eagle.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class Eagle : Enemy
 {
+    [SerializeField] private bool canAtack = false;
+    private float second;
+    [SerializeField] private GameObject EagleBullet;
+    private void Update()
+    {
+        if(canAtack)
+        {
 
+            second += Time.deltaTime;
+
+
+            if (second >= 1.0)    //1秒おきに攻撃
+            {
+                //memo:x=-2.55y=-3.5
+                GameObject g = Instantiate(EagleBullet);
+                g.transform.position = EagleBullet.transform.position;
+                g.transform.rotation = EagleBullet.transform.rotation;
+                g.SetActive(true);
+
+                second = 0;
+            }
+        }
+
+    }
 }
