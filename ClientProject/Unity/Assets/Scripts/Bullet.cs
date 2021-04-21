6790bcbf80c4801a6344b064b673af5d599a7319
DESKTOP-1T5ODT0\USER <jacktoaburaage@gmail.com>
2021-04-22 03:56:22 +0900

    Eagleコードの見直しと、Bullet関連の実装。もしかしたらまだ見直さないといけない部分があるかもしれないし、ないかもしれない。

---
+++ ClientProject/Unity/Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    private float activetime;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        activetime += Time.deltaTime;
+        if(activetime >=2.0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
