1e4358dd111a2fe49e6a82ebbb317d3f5e9708b0
DESKTOP-1T5ODT0\USER <jacktoaburaage@gmail.com>
2021-04-23 01:40:47 +0900

    Eagleの攻撃が凶悪すぎたので下方修正、新規EnemyのOpssumを追加。

---
+++ ClientProject/Unity/Assets/Scripts/Opossum.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opossum : Enemy
+{
+
+    [SerializeField] private int moveSpeed = 5;
+    // Start is called before the first frame update
+    //protected override void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+    }
+}
