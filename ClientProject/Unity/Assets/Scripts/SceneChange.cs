50548674ea5ec6be8a33eb028dd034d293177d0a
OiC-b8073-Ochi <b8073@oic.jp>
2021-04-23 17:59:14 +0900

    ステージ2-2枠組のみ作成 ボスの挙動、シーン移動のシステムを変更(移動時に上を入力して移動、EndSceneに移動するときはハーフシーン飛びません)

--- ClientProject/Unity/Assets/Scripts/SceneChange.cs
+++ ClientProject/Unity/Assets/Scripts/SceneChange.cs
@@ -6,16 +6,39 @@ using UnityEngine.SceneManagement;
 public class SceneChange : MonoBehaviour
 {
     [SerializeField] private string sceneName;
+    [HideInInspector] public bool canStageChange;
+
+    private void Update()
+    {
+        if (canStageChange == true)
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+            {
+
+                if (sceneName == "EndScene")
+                {
+                    SceneManager.LoadScene("EndScene");
+                }
+                else
+                {
+                    PermanentUI.perm.nowScene = sceneName;
+                    PermanentUI.perm.SceneName.text = sceneName;
+                    SceneManager.LoadScene("HalfScene");
+                }      
+            }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            //SceneManager.LoadScene("sceneName");
-            PermanentUI.perm.nowScene = sceneName;
-            PermanentUI.perm.SceneName.text = sceneName; 
-            SceneManager.LoadScene("HalfScene");
-
+            canStageChange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            canStageChange = false;
         }
     }
 }
