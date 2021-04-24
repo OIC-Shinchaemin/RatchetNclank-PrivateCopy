9e52676e23191ced1a5990fc6997b2160a33b448
darkstars-hub <79241161+darkstars-hub@users.noreply.github.com>
2021-04-24 20:01:56 +0900

    Stage一部修正、組み込み デバッグ開始　

---
+++ ClientProject/Mof/RachetNClankCopy/RachetApplication/Stage/WoodBox.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "GimmickBase.h"
+
+class WoodBox;
+using WoodBoxPtr   = std::shared_ptr<WoodBox>;
+using WoodBoxArray = std::vector<WoodBoxPtr>;
+
+class WoodBox : public GimmickBase {
+protected:
+
+    bool          _action_flag;
+
+    float         _gravity;
+
+    Vector3       _start_pos;
+
+    MeshPtr       _stage_mesh;
+
+    MeshArray*    _mesh_array_pointer;
+
+    WoodBoxArray* _box_array_pointer;
+
+    bool CollisionStage(void);
+
+    bool CollisionBox(WoodBoxPtr& box);
+
+public:
+
+    WoodBox(bool enable = true, bool collision = true,
+        StageObjectType type = StageObjectType::None, std::string name = "",
+        int mesh_no = -1,
+        Vector3 pos = Vector3(),
+        Vector3 scale = Vector3(1, 1, 1),
+        Vector3 rotate = Vector3());
+
+    ~WoodBox(void);
+
+    virtual void Initialize(void) override;
+    virtual void Update(float delta) override;
+
+    virtual void ActionStart(void) override;
+    virtual bool IsStart(void) const override;
+    virtual void SetStageObjectData(bool enable, bool collision, StageObjectType type, std::string name, int mesh_no,
+        Vector3 pos, Vector3 scale, Vector3 rotate) override;
+
+    void SetStageMesh(MeshPtr& mesh);
+    void SetBoxArray(WoodBoxArray* array_ptr);
+    void SetMeshArray(MeshArray* array_ptr);
+};
