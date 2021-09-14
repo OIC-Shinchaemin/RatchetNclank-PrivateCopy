f6fdb4a0858707e49b371b5fbb3059a8212f1baa
darkstars-hub <79241161+darkstars-hub@users.noreply.github.com>
2021-09-15 08:29:06 +0900

    text event create

--- ClientProject/Mof/RatchetNClankCopy/RatchetApplication/Actor/Character/King.cpp
+++ ClientProject/Mof/RatchetNClankCopy/RatchetApplication/Actor/Character/King.cpp
@@ -1,87 +1,109 @@
 #include "King.h"
 
+#include "../../Scene/GameScene.h"
 #include "../../Component/VelocityComponent.h"
 #include "../../Component/MeshComponent.h"
+#include "../../Factory/FactoryManager.h"
 
 
 ratchet::actor::character::King::King() :
-	super() {
+    super(),
+    _actor_container(),
+    _quest_index() {
 }
 
 ratchet::actor::character::King::~King() {
 }
 
 void ratchet::actor::character::King::OnNotify(const ratchet::actor::character::ScarecrowEndMessage&) {
-	auto message = ratchet::game::gamesystem::text::TextSystemMessage();
-	message.type = decltype(message.type)::EventNo0;
-	_text_system_message_subject.Notify(message);
+    auto message = ratchet::game::gamesystem::text::TextSystemMessage();
+    message.type = decltype(message.type)::EventNo0;
+    _text_system_message_subject.Notify(message);
 }
 
 void ratchet::actor::character::King::SetTexture(const std::shared_ptr<Mof::CTexture>& ptr) {
-	this->_question_texture = ptr;
+    this->_question_texture = ptr;
 }
+void ratchet::actor::character::King::SetGameScene(const std::shared_ptr<scene::GameScene>& ptr) {
+    this->_actor_container = ptr;
+}
+
 bool ratchet::actor::character::King::Initialize(ratchet::actor::Actor::Param* param) {
-	super::Initialize(param);
+    super::Initialize(param);
 
-	auto velocity = super::GetComponent<ratchet::component::VelocityComponent>();
-	velocity->Inactivate();
-	velocity->SetUseGravity(false);
+    auto velocity = super::GetComponent<ratchet::component::VelocityComponent>();
+    velocity->Inactivate();
+    velocity->SetUseGravity(false);
 
-	auto mesh = super::GetComponent<ratchet::component::MeshComponent>();
-	auto motion = mesh->GetMeshContainer()->CreateMotionController();
-	auto count = motion->GetMotionCount();
-	return true;
+    auto mesh = super::GetComponent<ratchet::component::MeshComponent>();
+    auto motion = mesh->GetMeshContainer()->CreateMotionController();
+    auto count = motion->GetMotionCount();
+    return true;
 }
 
 bool ratchet::actor::character::King::Update(float delta_time) {
-	super::Update(delta_time);
-
-	/*
-	if (::g_pInput->IsKeyPush(MOFKEY_O)) {
-		auto message = ratchet::game::gamesystem::text::TextSystemMessage();
-		message.type = decltype(message.type)::EventNo0;
-		_text_system_message_subject.Notify(message);
-	} // if
-	*/
-	return true;
+    super::Update(delta_time);
+    return true;
 }
 
 bool ratchet::actor::character::King::Render(void) {
-	super::Render();
+    super::Render();
 
-	// •`‰æ
-	auto pos = super::GetPosition();
-	float height = 2.4f;
-	auto transform = def::Transform(Mof::CVector3(pos.x, pos.y + height, pos.z));
-	if (auto tex = _question_texture.lock(); tex) {
-		Mof::CMatrix44 scale, rotate, translate;
-		Mof::CQuaternion quat; quat.Rotation(transform.rotate);
+    // •`‰æ
+    auto pos = super::GetPosition();
+    float height = 2.4f;
+    auto transform = def::Transform(Mof::CVector3(pos.x, pos.y + height, pos.z));
+    if (auto tex = _question_texture.lock(); tex) {
+        Mof::CMatrix44 scale, rotate, translate;
+        Mof::CQuaternion quat; quat.Rotation(transform.rotate);
 
-		scale.Scaling(transform.scale, scale);
-		quat.ConvertMatrixTranspose(rotate);
-		translate.Translation(transform.position, translate);
+        scale.Scaling(transform.scale, scale);
+        quat.ConvertMatrixTranspose(rotate);
+        translate.Translation(transform.position, translate);
 
-		Mof::CMatrix44 world = scale * rotate * translate;
-		auto camera = ::CGraphicsUtilities::GetCamera();
-		tex->Render(camera->GetBillBoardMatrix() * world);
+        Mof::CMatrix44 world = scale * rotate * translate;
+        auto camera = ::CGraphicsUtilities::GetCamera();
+        tex->Render(camera->GetBillBoardMatrix() * world);
 
-		//CGraphicsUtilities::RenderTexture();
-		//tex->Render(world, _rectangle.value());
-	} // if
-	return true;
+        //CGraphicsUtilities::RenderTexture();
+        //tex->Render(world, _rectangle.value());
+    } // if
+    return true;
 }
 
 void ratchet::actor::character::King::Talk(void) {
-	static int a = 0;
-	if (a == 0) {
-		auto message = ratchet::game::gamesystem::text::TextSystemMessage();
-		message.type = decltype(message.type)::EventNo0;
-		_text_system_message_subject.Notify(message);
-	} // if
-	else {
-		auto message = ratchet::game::gamesystem::text::TextSystemMessage();
-		message.type = decltype(message.type)::EventNo1;
-		_text_system_message_subject.Notify(message);
-	} // else
-	a++;
+    static int a = 0;
+    auto param = new ratchet::actor::Actor::Param();
+
+    if (a != 2) {
+        auto message = ratchet::game::gamesystem::text::TextSystemMessage();
+        message.type = decltype(message.type)::EventNo0;
+        _text_system_message_subject.Notify(message);
+
+        auto out = _actor_container.lock();
+
+                // npc
+        def::Transform scarecrow_transforms[]{
+            def::Transform(Mof::CVector3(-12.0f, -5.0f, -4.0f)),
+        };
+        param->tag = "scarecrow";
+        param->name = "scarecrow_1";
+        for (auto& transform : scarecrow_transforms) {
+            param->transform.position = transform.position;
+            param->transform.rotate = transform.rotate;
+            param->transform.scale = transform.scale;
+            auto scarecrow = ratchet::factory::FactoryManager::Singleton().CreateActor < ratchet::actor::character::Scarecrow>("../Resource/builder/scarecrow.json", param);
+            scarecrow->GetScarecrowEndMessageSubject()->AddObserver(std::dynamic_pointer_cast<ratchet::actor::character::King>(shared_from_this()));
+            out->AddElement(scarecrow);
+        } // for
+    } // if
+    else {
+        auto message = ratchet::game::gamesystem::text::TextSystemMessage();
+        message.type = decltype(message.type)::EventNo1;
+        _text_system_message_subject.Notify(message);
+    } // else
+    a++;
+
+    ut::SafeDelete(param);
+
 }
--- ClientProject/Mof/RatchetNClankCopy/RatchetApplication/Actor/Character/King.h
+++ ClientProject/Mof/RatchetNClankCopy/RatchetApplication/Actor/Character/King.h
@@ -8,62 +8,77 @@
 #include "Scarecrow.h"
 
 
+namespace ratchet::scene {
+class GameScene;
+}
 namespace ratchet::actor::character {
-	class King :
-		public actor::Actor,
-		public ratchet::actor::character::ScarecrowEndMessageListener {
-		using super = actor::Actor;
-	private:
-		//! ハテナ
-		std::weak_ptr<Mof::CTexture> _question_texture;
-		//! 通知用
-		ratchet::game::gamesystem::text::TextSystemMessageSubject _text_system_message_subject;
-	public:
-		/// <summary>
-		/// コンストラクタ
-		/// </summary>
-		King();
-		/// <summary>
-		/// デストラクタ
-		/// </summary>
-		virtual ~King();
-		/// <summary>
-		/// 通知
-		/// </summary>
-		/// <param name="message"></param>
-		virtual void OnNotify(const ratchet::actor::character::ScarecrowEndMessage& message);
-		/// <summary>
-		/// セッター
-		/// </summary>
-		/// <param name="ptr"></param>
-		void SetTexture(const std::shared_ptr<Mof::CTexture>& ptr);
-
-		auto GetTextSystemMessageSubject(void) {
-			return &this->_text_system_message_subject;
-		}
-		/// <summary>
-	/// 初期化
-	/// </summary>
-	/// <param name="param"></param>
-	/// <returns></returns>
-		virtual bool Initialize(ratchet::actor::Actor::Param* param) override;
-		/// <summary>
-		/// 更新
-		/// </summary>
-		/// <param name=""></param>
-		/// <returns></returns>
-		virtual bool Update(float delta_time) override;
-
-		/// <summary>
-	/// 描画
-	/// </summary>
-	/// <param name=""></param>
-	/// <returns></returns>
-		virtual bool Render(void) override;
-		/// <summary>
-		/// 会話
-		/// </summary>
-		void Talk(void);
-	};
+class King :
+    public actor::Actor,
+    public ratchet::actor::character::ScarecrowEndMessageListener {
+    using super = actor::Actor;
+private:
+    //! ハテナ
+    std::weak_ptr<Mof::CTexture> _question_texture;
+    //! 通知用
+    ratchet::game::gamesystem::text::TextSystemMessageSubject _text_system_message_subject;
+    //! 
+    std::weak_ptr<ratchet::scene::GameScene> _actor_container;
+    //! 回数
+    int _quest_index;
+public:
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    King();
+    /// <summary>
+    /// デストラクタ
+    /// </summary>
+    virtual ~King();
+    /// <summary>
+    /// 通知
+    /// </summary>
+    /// <param name="message"></param>
+    virtual void OnNotify(const ratchet::actor::character::ScarecrowEndMessage& message);
+    /// <summary>
+    /// セッター
+    /// </summary>
+    /// <param name="ptr"></param>
+    void SetTexture(const std::shared_ptr<Mof::CTexture>& ptr);
+    /// <summary>
+    /// セッター
+    /// </summary>
+    /// <param name="ptr"></param>
+    void SetGameScene(const std::shared_ptr<scene::GameScene>& ptr);
+    /// <summary>
+    /// ゲッター
+    /// </summary>
+    /// <param name=""></param>
+    /// <returns></returns>
+    auto GetTextSystemMessageSubject(void) {
+        return &this->_text_system_message_subject;
+    }
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    /// <param name="param"></param>
+    /// <returns></returns>
+    virtual bool Initialize(ratchet::actor::Actor::Param* param) override;
+    /// <summary>
+    /// 更新
+    /// </summary>
+    /// <param name=""></param>
+    /// <returns></returns>
+    virtual bool Update(float delta_time) override;
+    /// <summary>
+    /// 描画
+    /// </summary>
+    /// <param name=""></param>
+    /// <returns></returns>
+    virtual bool Render(void) override;
+    /// <summary>
+    /// 会話
+    /// </summary>
+    void Talk(void);
+};
 }
 #endif // !RATCHET_ACTOR_CHARACTER_KING_H
